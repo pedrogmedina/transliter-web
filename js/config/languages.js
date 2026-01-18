710a3888e3c0da53d065226731aece1510502bba
pedrogmedina <58294231+pedrogmedina@users.noreply.github.com>
2026-01-19 00:36:53 +0100

    new refact design

--- js/config/languages.js
+++ js/config/languages.js
@@ -1,7 +1,7 @@
 export const LANGUAGES = {
-    AUTO: { code: 'auto', name: 'Detectar automáticamente' },
-    EN: { code: 'en', name: 'Inglés' },
-    ES: { code: 'es', name: 'Español' },
-    FR: { code: 'fr', name: 'Francés' },
-    DE: { code: 'de', name: 'Alemán' },
+    AUTO: { code: 'auto', name: 'Detect automatically' },
+    EN: { code: 'en', name: 'English' },
+    ES: { code: 'es', name: 'Spanish' },
+    FR: { code: 'fr', name: 'French' },
+    DE: { code: 'de', name: 'German' },
 };
