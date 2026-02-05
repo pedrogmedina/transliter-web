41bb977f85479c5c9a040ee03b0db6e1e68ba74e
pedrogmedina <58294231+pedrogmedina@users.noreply.github.com>
2026-02-05 22:49:16 +0100

    refactor headlinks component

--- components/headlinks/headlinks.js
+++ components/headlinks/headlinks.js
@@ -1,9 +1,29 @@
 class HeadLinks extends HTMLElement {
     async connectedCallback() {
-        const response = await fetch('./components/headlinks/headlinks.html');
-        const html = await response.text();
-        this.innerHTML = html;
+        try {
+            const response = await fetch('./components/headlinks/headlinks.html');
+            const html = await response.text();
 
+            const template = document.createElement('template');
+            template.innerHTML = html;
+
+            const content = template.content;
+            const headEl = content.querySelector('head');
+
+            if (headEl) {
+                while (headEl.firstChild) {
+                    document.head.appendChild(headEl.firstChild);
+                }
+            } else {
+                while (content.firstChild) {
+                    document.head.appendChild(content.firstChild);
+                }
+            }
+
+            this.remove();
+        } catch (err) {
+            console.error('Error loading headlinks:', err);
+        }
     }
 }
 
